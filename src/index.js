89d9f355c89b07cc1631064209da97f393212441
Ruslan Salimov <work@salimov-dev.ru>
2022-10-18 14:16:53 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+
+console.log('Hello World!');
+
+const jsImageHTML = document.createElement('img')
+jsImageHTML.className = 'js-image'
+jsImageHTML.src = JS_IMAGE
+
+document.body.append(jsImageHTML)
